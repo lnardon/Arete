6af9a3406834d657f146c4549b76afe1e166da8b
Lucas Nardon <lucaslnardon@gmail.com>
2026-03-08 14:52:39 -0300

    add habits model

---
+++ server/internal/models/habit.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Habit struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type HabitCompletion struct {
+	HabitID string `json:"habitId"`
+	Date    string `json:"date"` // YYYY-MM-DD
+}
